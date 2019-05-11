4d5524cee52a62943f6b7ec912b2477ed4037256
Simone <not.simone.work@outlook.com>
2019-05-11 19:11:20 +0200

    group response and private

--- install.sh
+++ install.sh
@@ -8,8 +8,7 @@ sudo apt install python3-pip
 #create the virtual env
 sudo apt install python3-venv
 python3 -m venv env
-source env/bin/activate
 
 #Install all libs required
 echo Check requrements
-pip3 install -r requirments 
+pip3 install -r requirements 
--- server.py
+++ server.py
@@ -5,25 +5,27 @@ bot = telegram_wrapper("token.cfg")
 
 def make_reply(msg):
     reply = None
-    if msg.upper() == "PT":
-        reply = msg.upper()
+    if msg is None:
+        return reply
+    _msg = str(msg).upper()
+    if _msg == "PT":
+        reply = _msg
     return reply
 
 
-def main():
-    update_id = None
-    while True:
-        updates = bot.get_updates(offset=update_id)
-        updates = updates["result"]
-        if updates:
-            for item in updates:
-                update_id = item["update_id"]
-                try:
-                    message = str(item["message"]["text"])
-                except:
-                    message = None
-                from_ = item["message"]["from"]["id"]
-                reply = make_reply(message)
-                bot.send_message(reply, from_)
-
-main()
+update_id = None
+while True:
+    updates = bot.get_updates(offset=update_id)
+    updates = updates["result"]
+    if updates:
+        for item in updates:
+            update_id = item["update_id"]
+            try:
+                message = str(item["message"]["text"])
+            except:
+                message = None
+            _chat = item["message"]["chat"]["id"]
+            _user = item["message"]["from"]["id"]
+            reply = make_reply(message)
+            bot.send_message(reply, _chat)
+            bot.send_message(reply, _user)
