8dee3fbca271ede7171940256c07328961ea2bee
not.simone <not.simone.scuola@outlook.com>
2020-02-22 15:00:08 +0100

    Coronavirus improvement

--- core/commands/coronavirus.py
+++ core/commands/coronavirus.py
@@ -4,7 +4,6 @@ from datetime import datetime
 from core.src.settings import (
     MSG_ON_SAME_CHAT
 )
-from pprint import pprint
 
 
 class Coronavirus(object):
@@ -17,13 +16,25 @@ class Coronavirus(object):
 
         self.arg = arg
 
+        self._sort = None
+        _vars = ['_sort']
+        for param in params:
+            name = '_{}'.format(param[0])
+            setattr(self, name, param[1])
+
         self.total_confirmed = 0
         self.total_recovered = 0
         self.total_deaths = 0
 
         self.country_data = {}
 
-    def web_scrapper(self):
+    def __sort_by_rank(self, rank_name, sort_key):
+
+        _sorted = sorted(self.country_data.items(), key=lambda x: x[1][sort_key], reverse=True)
+        for i, el in enumerate(_sorted):
+            self.country_data[el[0]][rank_name] = i
+
+    def __web_scrapper(self):
 
         section = 'services1.arcgis.com/0MSEUqKaxRlEPj5g/arcgis/rest/services/ncov_cases/FeatureServer/1/'
         query = 'where=Confirmed%20%3E%200&returnGeometry=false&spatialRel=esriSpatialRelIntersects&outFields=*'
@@ -43,7 +54,7 @@ class Coronavirus(object):
         for data in data_list:
             report = data.get('attributes')
 
-            county = report.get('Country_Region')
+            county = report.get('Country_Region').lower()
             date = datetime.fromtimestamp(report.get('Last_Update') / 1e3)
             confirmed = report.get('Confirmed')
             recovered = report.get('Recovered')
@@ -70,54 +81,110 @@ class Coronavirus(object):
                     'deaths': deaths,
                 }
 
-    def void_arg(self):
+        self.__sort_by_rank('rank_by_confirmed', 'confirmed')
 
-        self.web_scrapper()
+    @staticmethod
+    def __calculate_death_percentage(confirmed, deaths):
+        if confirmed is 0:
+            deathly_percentage = 0
+        else:
+            deathly_percentage = deaths / confirmed * 100 if deaths is not 0 else 0
+
+        return str(deathly_percentage)[:4]
+
+    def __build_country(self, country):
+        try:
+            d = self.country_data.get(country)
+            if d:
+                date = d.get('date').strftime("%H:%M %d-%m-%Y")
+                confirmed = d.get('confirmed')
+                rank_by_confirmed = d.get('rank_by_confirmed')
+                recovered = d.get('recovered')
+                deaths = d.get('deaths')
+
+                out = '**#{} {}**\n'.format(rank_by_confirmed+1, country.upper())
+
+                out += 'Ultimo aggiornamento: {}\n'.format(date)
+                out += 'Infetti: {}\n'.format(confirmed)
+                out += 'Morti: {}\n'.format(deaths)
+                out += 'Guariti: {}\n'.format(recovered)
+
+                deathly_percentage = self.__calculate_death_percentage(confirmed, deaths)
+                out += 'Mortalità attuale: {}%\n\n'.format(str(deathly_percentage)[:5])
+                return out
+            else:
+                return ''
+        except Exception as e:
+            print(e)
+            return ''
+
+    def __build_country_list(self, sort_key):
+        print(self.country_data.items())
+        _sorted = sorted(self.country_data.items(), key=lambda x: x[1][sort_key], reverse=True)
+
+        out = ''
+        for i, el in enumerate(_sorted):
+            print(el)
+            out += '**#{} {}**, {}: {}\n'.format(
+                i+1,
+                el[0].upper(),
+                sort_key,
+                self.country_data[el[0]][sort_key],
+            )
+
+        return out
+
+    def void_arg(self):
 
         out = '**Coronavirus Update**\n\n'
         out += 'Da dove vengono presi i dati:\n'
         out += 'https://gisanddata.maps.arcgis.com/apps/opsdashboard/index.html#/bda7594740fd40299423467b48e9ecf6\n\n'
-        out += 'Totale Confermati: {}\n'.format(self.total_confirmed)
+        out += 'Totale Infetti: {}\n'.format(self.total_confirmed)
         out += 'Totale Morti: {}\n'.format(self.total_deaths)
         out += 'Totale Guariti: {}\n'.format(self.total_recovered)
 
-        deathly_percentage = self.total_deaths / self.total_recovered * 100
+        deathly_percentage = self.__calculate_death_percentage(self.total_confirmed, self.total_deaths)
         out += 'La mortalità attuale è del {}%'.format(str(deathly_percentage)[:5])
 
         self.bot.send_message(out, MSG_ON_SAME_CHAT, parse_mode_en=True)
 
         # countries to stamp data
-        my_country = ['Mainland China', 'Italy', 'Japan', 'South Korea']
+        my_country = ['italy', 'japan']
+        out = ''
         for country in my_country:
-            try:
-                out = '**{}**\n\n'.format(country)
-                d = self.country_data.get(country)
-                if d:
-                    pprint(d)
-                    confirmed = d.get('confirmed')
-                    recovered = d.get('recovered')
-                    deaths = d.get('deaths')
+            out += self.__build_country(country)
+        self.bot.send_message(out, MSG_ON_SAME_CHAT, parse_mode_en=True)
 
-                    out += 'Confermati: {}\n'.format(confirmed)
-                    out += 'Morti: {}\n'.format(deaths)
-                    out += 'Guariti: {}\n'.format(recovered)
-
-                    if recovered is 0 and deaths is not 0:
-                        deathly_percentage = 100
-                    else:
-                        deathly_percentage = deaths / recovered * 100 if deaths is not 0 else 0
-
-                    out += 'La mortalità attuale è del {}%'.format(str(deathly_percentage)[:5])
-
-                    self.bot.send_message(out, MSG_ON_SAME_CHAT, parse_mode_en=True)
-            except Exception as e:
-                print(e)
-                continue
+    def custom_country(self, country):
+        out = self.__build_country(country)
+        self.bot.send_message(out, MSG_ON_SAME_CHAT, parse_mode_en=True)
 
     def run(self):
+
+        self.__web_scrapper()
+
+        try:
+            if self._sort.lower() == ('c' or 'confirmed' or 'infetti'):
+                self.bot.send_message(self.__build_country_list('confirmed'), MSG_ON_SAME_CHAT, parse_mode_en=True)
+                return
+
+            if self._sort.lower() == ('r' or 'recovered' or 'guariti'):
+                self.bot.send_message(self.__build_country_list('recovered'), MSG_ON_SAME_CHAT, parse_mode_en=True)
+                return
+
+            if self._sort.lower() == ('d' or 'deaths' or 'morti'):
+                self.bot.send_message(self.__build_country_list('deaths'), MSG_ON_SAME_CHAT, parse_mode_en=True)
+                return
+
+        except AttributeError:
+            pass
 
         chose = {
             '': self.void_arg,
         }
+        try:
+            chose[self.arg]()
+        except KeyError:
+            self.custom_country(self.arg.lower())
 
-        chose[self.arg]()
+
